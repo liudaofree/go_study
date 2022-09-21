f60b37a1e71bff4a3b01ffede6db87f6b1254f7a
liudaofree <GFW1206@163.com>
2022-09-21 22:21:49 +0800

    init go project

---
+++ src/course1.go
@@ -0,0 +1,11 @@
+package src
+
+import "fmt"
+
+func Matchab(numa, numb int) {
+	if numb > numb {
+		fmt.Println("a" + "大于" + "b")
+	} else {
+		fmt.Println("b" + "大于" + "a")
+	}
+}
---
+++ start.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go_study/src"
+)
+
+func main() {
+	fmt.Println("hello world")
+	src.Matchab(1, 3)
+}
